437a36400b16f960c847592806636cfd2d375843
Gaurav <gauravkshah312@gmail.com>
2023-02-19 17:46:42 +0530

    fixed env var

--- main.go
+++ main.go
@@ -6,21 +6,26 @@ import (
 
 	"github.com/GaurKS/todo-app/pkg/routes"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 
 	// Setting variables for app env
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	router := gin.Default()
 	router.Use(gin.Logger())
 	r := router.Group("/api")
 	routes.TodoRouter(r.Group("/todo"))
 
-	router.Run(":" + os.Getenv("PORT"))
+	port := os.Getenv("PORT")
+	if port == "" {
+	 	port = "8080"
+	}
+	if err := router.Run(":" + port); err != nil {
+	 	log.Panicf("error: %s", err)
+	}
 }
